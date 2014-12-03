34802d9f5204bfba3bb590cb110c5d351fe1c4d5
David Eränen <davideranen@hotmail.com>
2014-12-03 20:47:45 +0200

    Improved AI. - Introduced board control factor - Measuring power of minion by casting cost. - Decreased score of 1 point of health by 0.5 - All of these factor are obviously going to change as I make the AI more advanced.

--- CSHearth/src/cshearth/ai/BasicAI.cs
+++ CSHearth/src/cshearth/ai/BasicAI.cs
@@ -19,8 +19,28 @@ namespace CSHearth
 			}
 
 			int myMinionCount = gs.Board.GetMinionCount( gs.Me );
+			int oppoMinionCount = gs.Board.GetMinionCount( gs.Opponent );
 
-			return myHealth + myMinionCount - oppoHealth;
+			double boardControlFactor = 1.0;
+			if( myMinionCount > 0 && oppoMinionCount == 0 ) {
+				boardControlFactor = 1.5;
+			}
+
+			double oppoMinionValue = 0.0;
+			foreach( Minion minion in gs.Board.GetMinions( gs.Opponent ) ) {
+				oppoMinionValue += minion.Cost + 1.0; // TODO: Use BaseCost here
+			}
+
+			double myMinionValue = 0.0;
+			foreach( Minion minion in gs.Board.GetMinions( gs.Me ) ) {
+				myMinionValue += minion.Cost + 1.0; // TODO: Use BaseCost here
+			}
+
+			double healthScore = 0.5 * (myHealth - oppoHealth);
+			double minionScore = myMinionValue - oppoMinionValue;
+			minionScore *= boardControlFactor;
+
+			return healthScore + minionScore;
 		}
 
 
